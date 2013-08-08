35d2436c91a6f7a3de9db715063e06947a060f4d
xuehuihe <xuehuihe.java@gmail.com>
2013-08-08 19:40:35 +0800

    sdsd

---
+++ example/src/main/java/com/sencha/gxt/Test.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2013 BEIJING UNION VOOLE TECHNOLOGY CO., LTD
+ */
+package com.sencha.gxt;
+
+/**
+ * @author XuehuiHe
+ * @date 2013年8月8日
+ */
+public class Test {
+//	com.sencha.gwt.uibinder.rebind.UiBinderGenerator
+}
---
+++ example/src/test/java/example/Teset.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2013 BEIJING UNION VOOLE TECHNOLOGY CO., LTD
+ */
+package example;
+
+/**
+ * @author XuehuiHe
+ * @date 2013年8月8日
+ */
+public class Teset {
+
+}
