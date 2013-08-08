28a0b22557f641d5006db26e185707d63f37be01
xuehuihe <xuehuihe.java@gmail.com>
2013-08-08 17:28:11 +0800

    sdf

--- etl-web/src/main/java/xdata/etl/web/server/aspect/AuthorityAspect.java
+++ etl-web/src/main/java/xdata/etl/web/server/aspect/AuthorityAspect.java
@@ -11,6 +11,9 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
+import xdata.etl.web.shared.annotations.AuthenticationMethod;
+import xdata.etl.web.shared.annotations.AuthenticationService;
+import xdata.etl.web.shared.exception.IllegalMethodException;
 import xdata.etl.web.shared.exception.SharedException;
 
 @Component
@@ -23,9 +26,20 @@ public class AuthorityAspect implements Ordered {
 	}
 
 	public void detail(JoinPoint jp) {
+		Class<?> clazz = jp.getTarget().getClass();
 		MethodSignature signature = (MethodSignature) jp.getSignature();
 		Method m = signature.getMethod();
-		System.out.println(m.getDeclaringClass().getName());
+
+		AuthenticationMethod am = m.getAnnotation(AuthenticationMethod.class);
+		AuthenticationService as = clazz
+				.getAnnotation(AuthenticationService.class);
+		if (am == null || as == null) {
+			throw new IllegalMethodException(m.getName() + " is illegal method");
+		}
+		if (am.isOpen()) {
+			System.out.println("-----------open---------------");
+		}
+		System.out.println("权限组名: " + as.value() + "\t权限名: " + am.value());
 		System.out.println("--------------detail------------");
 	}
 
