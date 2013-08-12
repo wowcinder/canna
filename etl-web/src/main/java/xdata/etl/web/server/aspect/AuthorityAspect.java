0ea604c2a2dde9541a4615ff60cbe7b9fc1cf17b
xuehuihe <xuehuihe.java@gmail.com>
2013-08-12 15:08:36 +0800

    sdf

--- etl-web/src/main/java/xdata/etl/web/server/aspect/AuthorityAspect.java
+++ etl-web/src/main/java/xdata/etl/web/server/aspect/AuthorityAspect.java
@@ -2,6 +2,8 @@ package xdata.etl.web.server.aspect;
 
 import java.lang.reflect.Method;
 
+import javax.servlet.http.HttpSession;
+
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -11,6 +13,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import xdata.etl.web.server.util.HibernateBeanUtil;
 import xdata.etl.web.shared.annotations.AuthenticationMethod;
@@ -32,6 +36,13 @@ public class AuthorityAspect implements Ordered {
 	}
 
 	public void doAccessCheck(JoinPoint jp) {
+		if(RequestContextHolder.getRequestAttributes() == null){
+			//NO Session
+			return ;
+		}
+		ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+		HttpSession session = attr.getRequest().getSession();
+		System.out.println(session);
 		Class<?> clazz = jp.getTarget().getClass();
 		MethodSignature signature = (MethodSignature) jp.getSignature();
 		Method m = signature.getMethod();
