57a5bd66ceb9460f655737f1ca4093314d242840
xuehuihe <xuehuihe.java@gmail.com>
2013-08-15 22:22:39 +0800

    sda

--- etl-web/src/main/java/xdata/etl/web/server/dao/hbasemeta/HbaseTableVersionDaoImpl.java
+++ etl-web/src/main/java/xdata/etl/web/server/dao/hbasemeta/HbaseTableVersionDaoImpl.java
@@ -3,10 +3,16 @@
  */
 package xdata.etl.web.server.dao.hbasemeta;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import xdata.etl.web.server.dao.RpcDao;
+import xdata.etl.web.shared.entity.hbasemeta.HbaseTableColumn;
 import xdata.etl.web.shared.entity.hbasemeta.HbaseTableVersion;
 import xdata.etl.web.shared.exception.SharedException;
 
@@ -17,16 +23,24 @@ import xdata.etl.web.shared.exception.SharedException;
 @Repository
 public class HbaseTableVersionDaoImpl extends
 		RpcDao<Integer, HbaseTableVersion> implements HbaseTableVersionDao {
+	@SuppressWarnings("unchecked")
 	@Override
 	public HbaseTableVersion update(HbaseTableVersion v) throws SharedException {
 		v = super.update(v);
-//		HbaseTableVersion version = (HbaseTableVersion) getSession()
-//				.createCriteria(HbaseTableVersion.class)
-//				.add(Restrictions.not(Restrictions.in("columns", v.getColumns())))
-//				.add(Restrictions.idEq(v.getId())).uniqueResult();
-//		for (HbaseTableColumn hbaseTableColumn : version.getColumns()) {
-//			getSession().delete(hbaseTableColumn);
-//		}
+		Session s = getSession();
+		List<Integer> ids = new ArrayList<Integer>();
+		for (HbaseTableColumn hbaseTableColumn : v.getColumns()) {
+			ids.add(hbaseTableColumn.getId());
+		}
+		Criteria criteria = s.createCriteria(HbaseTableColumn.class).add(
+				Restrictions.eq("version", v));
+		if (ids.size() > 0) {
+			criteria.add(Restrictions.not(Restrictions.in("id", ids)));
+		}
+		List<HbaseTableColumn> toDeletes = criteria.list();
+		for (HbaseTableColumn hbaseTableColumn : toDeletes) {
+			s.delete(hbaseTableColumn);
+		}
 		return v;
 	}
 }
