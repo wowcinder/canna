8251a5fe71f01f367fad99af0c9fe304c14c3dbd
xuehuihe <xuehuihe.java@gmail.com>
2013-08-20 16:08:32 +0800

    sds

--- etl-web/src/test/java/xdata/etl/web/server/Test.java
+++ etl-web/src/test/java/xdata/etl/web/server/Test.java
@@ -3,40 +3,10 @@
  */
 package xdata.etl.web.server;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.lang.reflect.Array;
-import java.lang.reflect.Method;
-import java.util.List;
-import java.util.Set;
 
 /**
  * @author XuehuiHe
  * @date 2013年8月12日
  * 
  */
-public class Test {
-	
-	private List<String> strs;
-	private String[] strs2;
-	private Set<String> strs3;
-	
-	@Retention(RetentionPolicy.RUNTIME)
-	@Target(ElementType.METHOD)
-	@Inherited
-	public @interface KK {
-
-	}
-
-	@KK
-	public void test() {
-
-	}
-
-	public static void main(String[] args) throws SecurityException, NoSuchFieldException {
-	}
-
-}
+public class Test {}
